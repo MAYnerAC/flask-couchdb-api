6ff25a341a6f0a33918b003366b814c3033af0e1
Mayner <ma2020067145@virtual.upt.pe>
2024-10-26 03:09:40 -0500

    v0.1.6

--- movie_api/app.py
+++ movie_api/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restx import Api
+from flask_cors import CORS
 from resources.movie import movie_ns
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)  # Habilitar CORS *
 api = Api(app, version='1.0', title='Movie API', description='A simple Movie API')
 
-# Rutas
+# Registrar el namespace
 api.add_namespace(movie_ns, path='/movie')
 
 if __name__ == '__main__':
