a22864a7f36a3589e49caaf66beacf725517325b
Mayner <ma2020067145@virtual.upt.pe>
2024-10-26 00:42:08 -0500

    v0.0.5

--- movie_api/app.py
+++ movie_api/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restx import Api
-from resources.movie import movie_ns  # Añadir esta línea para importar el namespace
+from resources.movie import movie_ns
 
 app = Flask(__name__)
 api = Api(app, version='1.0', title='Movie API', description='A simple Movie API')
